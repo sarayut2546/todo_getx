a0c1223b5b94a480ad800afb281196b9e8e3db99
lacta <65143335@g.cmru.ac.th>
2025-03-13 10:39:09 +0700

    init

---
+++ lib/controllers/todo_controller.dart
@@ -0,0 +1,40 @@
+import 'package:get/get.dart';
+import 'package:todo_getx/models/todo_model.dart';
+import 'package:todo_getx/services/storage_service.dart';
+
+class TodoController extends GetxController {
+  var todoList = <TodoModel>[].obs;
+  StorageService storageService = StorageService();
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchTodos();
+  }
+
+  void fetchTodos() {
+    var todos = storageService.read('todoList');
+    if (todos != null) {
+      todoList.value = List<TodoModel>.from(
+        todos.map((x) => TodoModel.fromJson(x)),
+      );
+    }
+  }
+
+
+  void addTodo(String title, String subtitle) {
+    todoList.add(TodoModel(title, subtitle, false));
+    storageService.write('todoList', todoList.toJson());
+  }
+
+  void toggleTodo(int index) {
+    todoList[index].isDone = !todoList[index].isDone;
+    todoList.refresh();
+    storageService.write('todoList', todoList.toJson());
+  }
+
+  void deleteTodo(int index) {
+    todoList.removeAt(index);
+    storageService.write('todoList', todoList.toJson());
+  }
+}
---
+++ lib/views/login_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:todo_getx/controllers/auth_controller.dart';
+import 'package:todo_getx/views/register_view.dart';
+import 'package:todo_getx/widgets/app_text_field.dart';
+ 
+// ignore: must_be_immutable
+class LoginView extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+ 
+  AuthController authController = Get.put(AuthController());
+
+  LoginView({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('เข้าสู่ระบบ'),
+        backgroundColor: Colors.teal,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            AppTextField(label: 'อีเมล', controller: emailController),
+            SizedBox(
+              height: 10,
+            ),
+            AppTextField(
+              label: 'รหัสผ่าน',
+              controller: passwordController,
+              hideText: true,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  if (!GetUtils.isEmail(emailController.text)) {
+                    Get.snackbar(
+                      "Error",
+                      "กรุณากรอกรหัสผ่านให้ครบ อย่างน้อย 6 ตัว",
+                      backgroundColor: const Color.fromARGB(255, 216, 4, 4),
+                      colorText: Colors.amber,
+                    );
+                    return;
+                  }
+                  if (passwordController.text.length < 6) {
+                    Get.snackbar(
+                      "Error",
+                      "กรอก Password ดิ๊",
+                      backgroundColor: Colors.redAccent,
+                      colorText: Colors.black,
+                    );
+                    return;
+                  }
+ 
+                  authController.login(
+                    emailController.text,
+                    passwordController.text,
+                  );
+                },
+                child: const Text('เข้าสู่ระบบ')),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                Get.to(RegisterView());
+              },
+              child: Text("สมัครสมาชิก"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
